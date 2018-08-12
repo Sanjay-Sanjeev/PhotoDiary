b2ed978aa96796d382ca09efc53c65586455ddf3
Sanjay Sanjeev <sanjaysanjeev2010@gmail.com>
2018-08-12 09:00:37 +0000

    initial commit of PhotoDiary App

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static( __dirname + "/public" ));
+
+
+app.get("/", function(req, res){
+    
+    res.render("home");
+    
+});
+
+app.get("/photos", function(req, res){
+    res.render("photos");
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Photo Diary Server Started...");
+});
