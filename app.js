ac14d64253c9bf4c716c3c7fab326b6359df5f5d
Sanjay Sanjeev <sanjaysanjeev2010@gmail.com>
2018-08-15 11:47:12 +0000

    add new photo form page and style css newForm. Add new and create route in app.js for photos

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static( __dirname + "/public" ));
 
-
+// Index Route - Landing Page
 app.get("/", function(req, res){
     
     res.render("home");
     
 });
 
+
+// Index Route - show all photos
 app.get("/photos", function(req, res){
     res.render("photos");
 });
+
+// New Route - show new photo diary form
+app.get("/photos/new", function(req, res) {
+    res.render("new");
+});
+
+// Create Route - crete new photo diary and redirect somewhere
 
 
 
