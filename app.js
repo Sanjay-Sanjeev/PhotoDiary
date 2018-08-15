1390821fd92d94c232595ff9bccaf09ae979bdfb
Sanjay Sanjeev <sanjaysanjeev2010@gmail.com>
2018-08-15 12:21:37 +0000

    add body-parser package to fetch request parameters

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
 
 mongoose.connect("mongodb://localhost:27017/photo_diary", { useNewUrlParser: true });
 
+var PhotoSchema = mongoose.Schema({
+    
+    title: String,
+    image: String,
+    desc: String
+    
+});
+
+var Photo = mongoose.model( "Photo" , PhotoSchema  );
 
 app.set("view engine", "ejs");
 app.use(express.static( __dirname + "/public" ));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // Index Route - Landing Page
 app.get("/", function(req, res){
@@ -28,7 +40,11 @@ app.get("/photos/new", function(req, res) {
 
 // Create Route - crete new photo diary and redirect somewhere - POST
 app.post("/photos", function(req, res){
-    res.send("POST route");
+    
+    res.send("POST ROUTE");
+    console.log(req.body);
+    
+    
 });
 
 
